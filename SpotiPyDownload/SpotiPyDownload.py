7ad3eb1048d8916cbea17ea9ad8fb6c1dd7c12cf
Callebe Severo <callebesev@gmail.com>
2025-12-22 21:44:00 -0300

    Funcional

--- SpotiPyDownload/SpotiPyDownload.py
+++ SpotiPyDownload/SpotiPyDownload.py
@@ -1,6 +1,9 @@
 from spotdl import Spotdl
 
+# The default spotdl auth:
 spotdl = Spotdl(client_id="f8a606e5583643beaa27ce62c48e3fc1", client_secret="f6f4c8f73f0649939286cf417c811607")
+
 songURL = str(input('Insira a URL da Playlist/Música -> '))
 toDownload = spotdl.search([songURL])
-spotdl.download_songs(toDownload)
+a = spotdl.download_songs(toDownload)
+print(a)
