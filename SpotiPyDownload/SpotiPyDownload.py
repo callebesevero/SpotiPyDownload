aa3d3459f37df047a7edc7b51956ca14e9547bf6
Callebe Severo <callebesev@gmail.com>
2025-12-23 18:29:48 -0300

    Encoding (erro)

--- SpotiPyDownload/SpotiPyDownload.py
+++ SpotiPyDownload/SpotiPyDownload.py
@@ -3,10 +3,12 @@ from pathlib import Path
 from shutil import move
 from time import sleep
 import os
-import io
 import sys
 
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+os.system('chcp 65001 > nul')
+
+if sys.stdout.encoding != 'utf-8':
+    sys.stdout = open(sys.stdout.fileno(), mode='w', encoding='utf-8', buffering=1)
 
 print('Espere... Carregando ferramentas! 😁')
 
