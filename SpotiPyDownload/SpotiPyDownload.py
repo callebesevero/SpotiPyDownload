86746abc99f61ca72e33398e3998e708db047085
Callebe Severo <callebesev@gmail.com>
2025-12-22 21:59:20 -0300

    Continuar na captação da pasta Downloads

--- SpotiPyDownload/SpotiPyDownload.py
+++ SpotiPyDownload/SpotiPyDownload.py
@@ -1,9 +1,19 @@
 from spotdl import Spotdl
+from pathlib import Path
 
 # The default spotdl auth:
 spotdl = Spotdl(client_id="f8a606e5583643beaa27ce62c48e3fc1", client_secret="f6f4c8f73f0649939286cf417c811607")
 
+# Catch music URL
 songURL = str(input('Insira a URL da Playlist/Música -> '))
 toDownload = spotdl.search([songURL])
-a = spotdl.download_songs(toDownload)
-print(a)
+downloaded = spotdl.download_songs(toDownload)
+
+# Construct the path to \Downloads folder
+...
+
+# Catch the path of each downloaded song
+for music in downloaded:
+    if ...:
+        ...
+print(downloaded)
